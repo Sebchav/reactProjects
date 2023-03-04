b90cfff9ef7030a9427e4208cf27aa587d28afbb
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2023-03-03 22:24:20 -0600

    Error message and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react"
 import Header from "./components/Header"
 import Formulario from "./components/Formulario"
 import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
+
+  const {pacientes, setPacientes} = useState([]);
+
   return (
     <div className="container mx-auto mt-20">
-      <Header />
+      <Header 
+          numeros = {1}
+          isAdmin = {false}
+
+      />
 
       <div className="mt-12 md:flex">
         <Formulario />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Header = () => {
+const Header = (props) => {
   return (
         <h1 className='font-black text-5xl text-center md:w-2/3 mx-auto'>
           Seguimiento Pacientes {""}
